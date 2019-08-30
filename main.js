156d54da26db46c1ba39ca047fe3f815fd05487c
Cuong Nguyen <cuongnmph04158@gmail.com>
2019-08-30 10:47:02 +0700

    Update Code 2

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 const X = 10;
 const Y = 10;
 const WIDTH = 200;
-const HEIGH = 100;
+const HEIGHT = 100;
 const COLOR = "#000000";
 
 let Rectangle = function () {
     this.x = X;
     this.y = Y;
     this.width = WIDTH;
-    this.heigh = HEIGH;
+    this.height = HEIGHT;
     this.color = COLOR;
 }
 Rectangle.prototype.render = function(){
     let rec = document.createElement('canvas');
         rec.id = "rectangle";
         rec.width = WIDTH;
-        rec.height = HEIGH;
+        rec.height = HEIGHT;
         document.body.appendChild(rec);
     let _canvas = document.getElementById('rectangle');
     let context = _canvas.getContext("2d");
         context.beginPath();
         context.fillStyle = this.color;
-        context.fillRect(this.x,this.y,this.width,this.heigh);
+        context.fillRect(this.x,this.y,this.width,this.height);
         context.closePath();
 
 }
